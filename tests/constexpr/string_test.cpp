adf9b554df046652f531cf1342b84f80b8104a39
Pennywise007 <skacnep@gmail.com>
2023-12-01 18:16:26 +0100

    Separating types with constexpr support to a special folder. Adding constexpr string support

---
+++ tests/constexpr/string_test.cpp
@@ -0,0 +1,47 @@
+#include "gtest/gtest.h"
+
+#include <string_view>
+
+#include <ext/constexpr/string.h>
+
+using ext::constexpr_string;
+
+constexpr constexpr_string textFirst = "test";
+constexpr constexpr_string textSecond = "second";
+
+TEST(constexpr_string_test, text_and_size) {
+    static_assert(textFirst.str() == std::string_view("test"));
+    static_assert(textFirst.size() == 4);
+    static_assert(textSecond.str() == std::string_view("second"));
+    static_assert(textSecond.size() == 6);
+
+    constexpr constexpr_string emptyText = "";
+    static_assert(emptyText.str() == std::string_view(""));
+    static_assert(emptyText.size() == 0);
+}
+
+TEST(constexpr_string_test, chars) {
+    static_assert(textFirst[0] == 't');
+    static_assert(textFirst[1] == 'e');
+    static_assert(textFirst[2] == 's');
+    static_assert(textFirst[3] == 't');
+    static_assert(textFirst[4] == '\0');
+}
+
+TEST(constexpr_string_test, uniting_text) {
+    static_assert((textFirst + "_" + textSecond).str() == std::string_view("test_second"));
+    static_assert(("some_" + textFirst).str() == std::string_view("some_test"));
+}
+
+TEST(constexpr_string_test, equal_text) {
+    static_assert(constexpr_string("wow") == constexpr_string("wow"));
+    static_assert(constexpr_string("text") == "text");
+    static_assert("text" == constexpr_string("text"));
+}
+
+TEST(constexpr_string_test, not_equal_text) {
+    static_assert(constexpr_string("wow") != constexpr_string("waw"));
+    static_assert(constexpr_string("wow") != constexpr_string("wo"));
+    static_assert(constexpr_string("text") != "tost");
+    static_assert("text" != constexpr_string("tost"));
+}

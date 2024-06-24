6a164291e9cf3957a8a19a323a848107ad0dff51
Pennywise <s-kacnep@ya.ru>
2024-06-24 23:16:58 +0200

    Adding enum reflection

--- include/ext/utils/reflection.h
+++ include/ext/utils/reflection.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <type_traits>
+#include <string_view>
 
 #include <ext/details/reflection_details.h>
+
+#include <ext/std/source_location.h>
 
 namespace ext::reflection {
 
@@ -27,12 +30,141 @@ static_assert(ext::reflection::brace_constructor_size<TestStruct> == 2);*/
 template <typename T, size_t MaxConstructorSize = 100>
 inline constexpr size_t brace_constructor_size = ext::details::refletion::find_max_brace_constructor<T, MaxConstructorSize, 0>::Count() - 1;
 
-// Check if object got a field
-#define HAS_FIELD(Type, FieldName)                              \
-    []() {                                                      \
-        auto check = [](auto u) -> decltype(u.FieldName) {};    \
-        return std::is_invocable_v<decltype(check), Type>;      \
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Check if object has a field
+#define HAS_FIELD(Type, FieldName)                                      \
+    []() {                                                              \
+        constexpr auto check = [](auto u) -> decltype(u.FieldName) {};  \
+        return std::is_invocable_v<decltype(check), Type>;              \
     }()
 
+// Check if object has a function
+#define HAS_FUNCTION(Type, FunctionName)                                                \
+    []() {                                                                              \
+        constexpr auto check = [](auto u) -> decltype(&decltype(u)::FunctionName) {};   \
+        return std::is_invocable_v<decltype(check), Type>;                              \
+    }()
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Old Fashioned way to check field/function exist. Can be useful when you need to check private fields.
+
+// Declare checker for field existing, checks if object has field by name
+// Example:
+//      struct A { bool someField; };
+//      DECLARE_CHECK_FIELD_EXISTS(someField);
+//      static_assert(has_someField_field_v<A>);
+#define DECLARE_CHECK_FIELD_EXISTS(FieldName)                       \
+template<typename T>                                                \
+struct has_##FieldName##_field {                                    \
+    template<typename U, typename = decltype(std::declval<U>().FieldName)>  \
+    static std::true_type test(int);                                \
+    template<typename>                                              \
+    static std::false_type test(...);                               \
+    static constexpr bool value = decltype(test<T>(0))::value;      \
+};                                                                  \
+template<class T>                                                   \
+inline constexpr bool has_##FieldName##_field_v = has_##FieldName##_field<T>::value
+
+// Declare checker for function name, checks if object has function by name
+// Example:
+//      DECLARE_CHECK_FUNCTION(reserve);
+//      static_assert(has_reserve_function_v<std::vector<int>>);
+#define DECLARE_CHECK_FUNCTION_EXISTS(FunctionName)                 \
+template<typename Class, class = std::void_t<>>                     \
+struct has_##FunctionName##_function : std::false_type {};          \
+template<typename Class>                                            \
+struct has_##FunctionName##_function<Class, std::enable_if_t<std::is_member_function_pointer_v<decltype(&Class::FunctionName)>>> : std::true_type {}; \
+template<class T>                                                   \
+inline constexpr bool has_##FunctionName##_function_v = has_##FunctionName##_function<T>::value
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <auto EnumType>
+[[nodiscard]] constexpr std::string_view get_enum_name() {
+#if defined(__clang__) && defined(_MSC_VER)
+    // For Clang on Windows we use a compiler-specific macro
+    constexpr auto func_name = std::string_view{__PRETTY_FUNCTION__};
+#else
+#if _HAS_CXX20
+    constexpr auto func_name =
+        std::string_view{std::source_location::current().function_name()};
+#else // _HAS_CXX20
+    // For c++17 we need to use __builtin_FUNCSIG instead of __builtin_FUNCTION
+    constexpr auto func_name = std::string_view{__builtin_FUNCSIG()};
+#endif // _HAS_CXX20
+#endif // defined(__clang__) && defined(_MSC_VER)
+
+#if defined(__clang__) || defined(__GNUC__)
+    constexpr auto split = func_name.substr(0, func_name.size() - 1);
+    return split.substr(split.find("EnumType = ") + 4);
+#elif defined(_MSC_VER)
+    constexpr auto prefix = "get_enum_name<";
+    constexpr auto suffix = ">(void)";
+    constexpr auto split = func_name.substr(0, func_name.size() - std::string_view(suffix).size());
+    return split.substr(split.find(prefix) + std::string_view(prefix).size());
+#else
+    static_assert(false, "Unsupported compiler");
+#endif
+}
+
+template <class EnumType>
+constexpr bool is_scoped_enum_v = std::is_enum_v<EnumType> &&
+    !std::is_convertible_v<EnumType, std::underlying_type_t<EnumType>>;
+
+template <class EnumType, auto EnumValue>
+[[nodiscard]] constexpr bool is_enum_value()
+{
+    // value not belong to enum, example: "(enum TestEnum)0xa"
+    return get_enum_name<static_cast<EnumType>(EnumValue)>()[0] != '(';
+}
+
+template <class EnumType, int minValue, int maxValue, int i>
+[[nodiscard]] constexpr unsigned get_enum_size_impl()
+{
+    if constexpr (i > maxValue)
+        return 0;
+    else
+    {
+        if constexpr (!is_enum_value<EnumType, i>())
+            return get_enum_size_impl<EnumType, minValue, maxValue, i + 1>();
+        else
+            return 1 + get_enum_size_impl<EnumType, minValue, maxValue, i + 1>();
+    }
+}
+
+template <class EnumType, int indexToSearch, int startSearchValue = 0, int endSearchValue = 100, int i, unsigned foundItems>
+[[nodiscard]] constexpr EnumType get_enum_value_impl()
+{
+    if constexpr (i > endSearchValue)
+        return 0;
+    else
+    {
+        constexpr auto name = get_enum_name<static_cast<EnumType>(i)>();
+        if constexpr (!is_enum_value<EnumType, i>())
+            return get_enum_value_impl<EnumType, indexToSearch, startSearchValue, endSearchValue, i + 1, foundItems>();
+        else if constexpr (indexToSearch == foundItems)
+            return static_cast<EnumType>(i);
+        else
+            return get_enum_value_impl<EnumType, indexToSearch, startSearchValue, endSearchValue, i + 1, foundItems + 1>();
+    }
+}
+
+template <class EnumType, int startSearchValue = 0, int endSearchValue = 100>
+[[nodiscard]] constexpr unsigned get_enum_size()
+{
+    static_assert(is_scoped_enum_v<EnumType>, "Type is not a enum type");
+    static_assert(endSearchValue > startSearchValue, "Invalid params");
+
+    return get_enum_size_impl<EnumType, startSearchValue, endSearchValue, startSearchValue>();
+}
+
+template <class EnumType, unsigned searchIndex, int startSearchValue = 0, int endSearchValue = 100>
+[[nodiscard]] constexpr EnumType get_enum_value()
+{
+    static_assert(is_scoped_enum_v<EnumType>, "Type is not a enum type");
+    static_assert(endSearchValue > startSearchValue, "Invalid params");
+    static_assert(get_enum_size<EnumType, startSearchValue, endSearchValue>() > searchIndex, "Value with given index doesn't exist in enum");
+
+    return get_enum_value_impl<EnumType, searchIndex, startSearchValue, endSearchValue, startSearchValue, 0>();
+}
 
 } // namespace ext::reflection
--- tests/utils/reflection_test.cpp
+++ tests/utils/reflection_test.cpp
@@ -33,9 +33,57 @@ struct TestStruct  {
     std::string name1;
     std::string name2;
     std::string name3;
+
+    void existingFunction(int) {}
 };
 
 static_assert(ext::reflection::brace_constructor_size<TestStruct> == 4, "Failed to determine fields count");
 
 static_assert(HAS_FIELD(TestStruct, name), "Failed to find name field");
 static_assert(!HAS_FIELD(TestStruct, unknown), "Failed to find unknown field");
+
+static_assert(HAS_FUNCTION(TestStruct, existingFunction), "Failed to detect `existingFunction`");
+static_assert(!HAS_FUNCTION(TestStruct, unknownFunction), "Found not existingFunction");
+
+enum class TestEnum
+{
+    eEnumValue1,
+    eEnumValue2,
+    eEnumValue5 = 5,
+    eEnumValue101 = 101,
+};
+
+template <class T>
+struct TestClass
+{
+    enum class InternalEnum
+    {
+        eInternalEnumValue1,
+    };
+};
+
+static_assert(ext::reflection::get_enum_name<TestEnum(0)>() == "TestEnum::eEnumValue1",
+    "Failed to get enum value name");
+static_assert(ext::reflection::get_enum_name<TestEnum::eEnumValue2>() == "TestEnum::eEnumValue2",
+    "Failed to get enum value name");
+static_assert(ext::reflection::get_enum_name<TestEnum(101)>() == "TestEnum::eEnumValue101",
+    "Failed to get enum value name");
+static_assert(ext::reflection::get_enum_name<TestClass<int>::InternalEnum::eInternalEnumValue1>() == "TestClass<int>::InternalEnum::eInternalEnumValue1",
+    "Failed to get internal enum value name");
+    
+static_assert(ext::reflection::is_enum_value<TestEnum, TestEnum::eEnumValue1>(),
+    "Failed to detect if value belongs to enum");
+static_assert(ext::reflection::is_enum_value<TestEnum, TestEnum::eEnumValue2>(),
+    "Failed to detect if value belongs to enum");
+static_assert(!ext::reflection::is_enum_value<TestEnum, -100>(),
+    "Failed to detect if value belongs to enum");
+    
+static_assert(ext::reflection::get_enum_size<TestEnum>() == 3,
+    "Failed to calc enum size");
+static_assert(ext::reflection::get_enum_size<TestEnum, 0, 101>() == 4,
+    "Failed to calc enum size with 101 elements");
+
+static_assert(ext::reflection::get_enum_value<TestEnum, 0>() == TestEnum::eEnumValue1);
+static_assert(ext::reflection::get_enum_value<TestEnum, 1>() == TestEnum::eEnumValue2);
+static_assert(ext::reflection::get_enum_value<TestEnum, 2>() == TestEnum::eEnumValue5);
+static_assert(ext::reflection::get_enum_value<TestEnum, 3, 0, 101>() == TestEnum::eEnumValue101);
